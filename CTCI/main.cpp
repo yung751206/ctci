c861f90a5add1c8ada1c4e0722a2461309e4bbe6
Edison <Edison@Edisons-MacBook-Pro.local>
2014-06-24 22:57:26 +0900

    Initial Commit

---
+++ CTCI/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  CTCI
+//
+//  Created by Edison on 6/24/14.
+//  Copyright (c) 2014 Edison. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
